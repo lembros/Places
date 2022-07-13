220b8229b13a000e37869ef7bcc9259c2d06bfe1
George <george85051@gmail.com>
2022-07-13 22:49:53 +0300

    Design changes and keyboard behavior

--- DataInputScreen.swift
+++ DataInputScreen.swift
@@ -12,19 +12,41 @@ class DataInputScreen: UITableViewController {
     @IBOutlet weak var typePicker: UIPickerView!
     @IBOutlet weak var placeNameField: UITextField!
     @IBOutlet weak var locationField: UITextField!
+    @IBOutlet weak var image: UIImageView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        placeNameField.returnKeyType = .done
+        locationField.returnKeyType = .done
+        
+        placeNameField.delegate = self
+        locationField.delegate = self
+        
         typePicker.dataSource = self
         typePicker.delegate = self
     }
 
 
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        0
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0: break
+        default: view.endEditing(true)
+        }
+    }
 }
 
-
+extension DataInputScreen: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
 
 extension DataInputScreen: UIPickerViewDataSource {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {

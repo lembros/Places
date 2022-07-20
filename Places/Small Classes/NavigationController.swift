4f29bf1c1eb648eca032caea43c1f8c40941f1f4
George <george85051@gmail.com>
2022-07-20 19:54:17 +0400

    Returned deleted files

---
+++ Places/Small Classes/NavigationController.swift
@@ -0,0 +1,24 @@
+//
+//  NavigationController.swift
+//  
+//
+//  Created by Егор Губанов on 20.07.2022.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        self.presentationController?.delegate = self
+    }
+}
+
+extension NavigationController: UIAdaptivePresentationControllerDelegate {
+    // Just to deselect the cell when DataInputScreen is dismissed
+    func presentationControllerWillDismiss(_ presentationController: UIPresentationController) {
+        let dataScreen = children.first! as! DataInputScreen
+        
+        dataScreen.performSegue(withIdentifier: "dismissDataScreen", sender: dataScreen)
+    }
+}
